d58ed492a725e96718e9f0142992f27d3d396816
Eric Nguyen <eric+github@ericnguyen.com>
2011-04-06 09:26:14 -0700

    Added this year's baroot images, twitter widget committed to repository for safekeeping, various styling fixes.

---
+++ js/twitterwidget.js
@@ -0,0 +1,38 @@
+<!-- These are the contents of the twitter script to be added as
+     a to the Widget to the sidebar.
+-->
+
+<script src="http://widgets.twimg.com/j/2/widget.js"></script>
+<script>
+		new TWTR.Widget({
+				version: 2,
+				type: 'faves',
+				rpp: 10,
+				interval: 6000,
+				title: '',
+				subject: '',
+				width: 'auto',
+				height: 300,
+				theme: {
+						shell: {
+								background: 'none',
+								color: '#ffffff'
+						},
+						tweets: {
+								background: 'none',
+								color: '#fff',
+								links: '#fff'
+						}
+				},
+				features: {
+						scrollbar: false,
+						loop: false,
+						live: true,
+						hashtags: true,
+						timestamp: true,
+						avatars: true,
+						toptweets: true,
+						behavior: 'all'
+				}
+		}).render().setUser('falseprofitllc').start();
+</script>
